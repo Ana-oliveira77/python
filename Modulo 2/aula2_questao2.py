4d9fd5e270b8cb3434bfbd565515fe7001341dc1
Ana-oliveira77 <ana.oliveira@pditabira.com>
2025-04-27 17:29:05 -0300

    exercicios 1 ao 4 - aula 2

---
+++ Modulo 2/aula2_questao2.py
@@ -0,0 +1,12 @@
+pop = "o resultado é "
+num1 = 10
+num2 = 3.5
+num3 = num1+num2
+print(pop)
+print(num1)
+print(num2)
+print(num3)
+print(type(pop))
+print(type(num1))
+print(type(num2))
+print(type(num3))
