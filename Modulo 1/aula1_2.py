80d767da6168c04a270000b329a6257c7b92f3aa
Ana-oliveira77 <ana.oliveira@pditabira.com>
2025-04-27 17:22:42 -0300

    modulo 1

---
+++ Modulo 1/aula1_2.py
@@ -0,0 +1,14 @@
+# Exemplo 01
+print("Meu primeiro código")
+
+# Exemplo 02
+x = 10
+y = 5
+print(x + 1)
+print(y - 2)
+print(y * x)
+print(x / y)
+
+# Exemplo 03 (com erro de digitação proposital)
+print("Ola")
+
